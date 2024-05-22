7597da51e7d4b3411f94b56bbc13d1d551f0191f
Nmcp1 <141276162+Nmcp1@users.noreply.github.com>
2024-05-22 00:29:53 -0400

    v1

--- pivote/models.py
+++ pivote/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    mail = models.CharField(max_length=35)
+    password = models.CharField(max_length=35)
+
+    def __str__(self):
+        m2= ""
+        for letra in self.mail:
+            if (letra!="@"):
+                m2+=letra
+            else:
+                break
+        return m2
--- pivote/views.py
+++ pivote/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-
     return render(request, "pivote/home.html")
+
+def login(request):
+    if request.method == "POST":
+        contraseña = request.POST.get("contraseña")
+        mail = request.POST.get("mail")
+        try:
+            usuario_selec = Usuario.objects.get(mail=mail)
+            if(str(contraseña) == str(usuario_selec.password)):
+                datos={}
+                datos["nombre"]=usuario_selec.__str__
+                return render(request, 'pivote/initial.html',datos)
+            else:
+                messages.error(request, "ERROR: Contraseña incorrecta, por favor vuelva a intentarlo.")
+                return render(request, 'pivote/home.html')
+        except Usuario.DoesNotExist:
+            messages.error(request, "ERROR: Cuenta no existente, por favor vuelva a intentarlo.")
+            return render(request, 'pivote/home.html')
