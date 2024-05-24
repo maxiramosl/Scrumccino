3e798c87344a49ae99cdcbcb2192bfb530f330b8
Zada <Zada.riquelme@usm.cl>
2024-05-23 23:11:31 -0400

    Programa Historia de Registro Usuario

--- mocapp/urls.py
+++ mocapp/urls.py
@@ -20,5 +20,6 @@ from pivote import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('register/', views.register, name='register'),
 ]
