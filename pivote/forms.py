1a9a9b42e20d42b087571e33d660f92687863b0d
whatebria <jennifer.castillo@usm.cl>
2024-05-26 20:14:23 -0400

    entrega final

--- pivote/forms.py
+++ pivote/forms.py
@@ -1,9 +1,9 @@
+from typing import Any
 from django import forms  
 from django.contrib.auth.models import User  
 from django.contrib.auth.forms import UserCreationForm  
 from django.core.exceptions import ValidationError  
-from django.forms.fields import EmailField  
-from django.forms.forms import Form 
+
 
 class CustomUserCreationForm(UserCreationForm):  
     username = forms.CharField(label='Username', min_length=5, max_length=150)  
@@ -40,3 +40,4 @@ class CustomUserCreationForm(UserCreationForm):
             self.cleaned_data['password1']  
         )  
         return user  
+
--- pivote/views.py
+++ pivote/views.py
@@ -1,55 +1,62 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from .forms import CustomUserCreationForm  
-from django.contrib.auth import authenticate, login
+from .forms import CustomUserCreationForm
 
 from django.conf import settings
 
 from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
 
+    valid = request.GET.get('boton')
+    if valid == 'valido':
+        return('true')
+
     return render(request, "pivote/home.html")
 
 @login_required
 def plataforma(request):
+    
     return render(request, 'pivote/plataforma.html')
 
 def exit(request):
     logout(request)
     return redirect('home')
 
-def send_email(email):
-    context = {'email': email}
+def send_email(email, name):
+
+    context = {'email': email,
+               'name': name,
+               }
     template = get_template('pivote/correo.html')
     content = template.render(context)
 
     email = EmailMultiAlternatives(
-        'Titulo correo',
-        'Menaje',
+        'Correo de validación',
+        'Pico',
         settings.EMAIL_HOST_USER,
-        [email]
+        [email],
     )
-
     email.attach_alternative(content, 'text/html')
     email.send()
 
-
 def register(request):  
     if request.method == 'POST':  
-        form = CustomUserCreationForm(data = request.POST)  
-        if form.is_valid(): 
+        form = CustomUserCreationForm(data = request.POST)
+        
+        if form.is_valid() and form.clean_password2 and form.username_clean and form.email_clean: 
             email = request.POST.get('email')
-            send_email(email) 
+            name = request.POST.get('username')
+            send_email(email, name) 
             form.save()  
-            return redirect('home')
-    else:  
+    else: 
         form = CustomUserCreationForm()  
     context = {  
         'form':form  
     }  
-    return render(request, 'registration/register.html', context)  
+    return render(request, 'registration/register.html', context)
