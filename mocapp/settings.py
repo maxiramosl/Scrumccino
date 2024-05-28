25e49438e6deca2e2b9f5da8efa9f1e4261bf918
mobo <maximiliano.ramosl@usm.cl>
2024-05-27 20:52:25 -0400

    TERMINADO

--- mocapp/settings.py
+++ mocapp/settings.py
@@ -124,6 +124,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND='django.core.backends.smtp.EmailBackend'
-EMAIL_HOST='scrumccino@gmail.com'
 
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'scrumccino@gmail.com'
+EMAIL_HOST_PASSWORD = 'ymmq qafw gdkn kjfc'
+EMAIL_USE_TLS = True
