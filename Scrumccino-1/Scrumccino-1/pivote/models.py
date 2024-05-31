12b584928512d176cf4e6e23f2cc2b900e4ffeb7
FranCapelli <119713583+FranCapelli@users.noreply.github.com>
2024-05-31 05:48:12 -0400

    Delete Scrumccino-1/Scrumccino-1 directory

--- Scrumccino-1/Scrumccino-1/pivote/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class CustomUser(User):
-    correo=models.CharField(max_length=100, blank=False)
-    contraseña=models.CharField(max_length=100, )
-
-
-# Create your models here.
