78ce51445685f6c220e8e27671c4fcddd900e893
Zada <Zada.riquelme@usm.cl>
2024-07-03 20:30:19 -0400

    acceso-prototipo

--- mocapp/urls.py
+++ mocapp/urls.py
@@ -20,6 +20,7 @@ from pivote import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
+    path('', views.acceso, name='acceso'),  # URL raíz apunta a la vista de acceso
     path('register/', views.register, name='register'),
+    path('main/', views.main_page, name='main_page'),  # Página principal después de iniciar sesión
 ]
--- pivote/views.py
+++ pivote/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .forms import UserRegistrationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserRegistrationForm, LoginForm
 
 def home(request):
     if request.method == 'POST':
@@ -20,3 +21,25 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'pivote/home.html', {'form': form})
+
+
+def acceso(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('main_page')  # Cambia 'main_page' por el nombre de tu vista principal
+            else:
+                return render(request, 'pivote/acceso.html', {'form': form, 'message': 'Correo o contraseña incorrectos.'})
+    else:
+        form = LoginForm()
+    return render(request, 'pivote/acceso.html', {'form': form})
+
+
+def main_page(request):
+    # Aquí deberías definir la lógica y plantilla para la página principal a la que los usuarios registrados serán redirigidos.
+    return render(request, 'pivote/main_page.html')
