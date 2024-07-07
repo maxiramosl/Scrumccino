b12387432c775d1bd8d79909965fc9e9720ea456
Zada <Zada.riquelme@usm.cl>
2024-07-06 22:46:50 -0400

    ...

--- mocapp/urls.py
+++ mocapp/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pivote import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
